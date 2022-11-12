d60c6fae7249ff132d7b1ae08842f3ed8a01d22a
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-11-11 19:15:58 -0500

    finished connection.js file

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+// dependencies
+const Sequelize = require('sequelize');
+
+// username and password
+require('dotenv').config();
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
+module.exports = sequelize;
