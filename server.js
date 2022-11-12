226e7c4cad19a2c1ae9a27d7bf390ccc2774010c
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-11-11 23:54:15 -0500

    initial server.js content, renamed some files

---
+++ server.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
+
+// const sess = {
+//     secret: '',
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
+app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening at localhost:${PORT}`))
+});
