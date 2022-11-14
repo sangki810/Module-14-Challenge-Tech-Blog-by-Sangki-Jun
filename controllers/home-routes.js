83021afa2db9457500823797b30d6a45a4900472
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-11-14 10:59:59 -0500

    added inital home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,26 +4,71 @@ const { Post, Comment, User } = require('../models/');
 // get all posts for homepage
 router.get('/', async (req, res) => {
     // post findAll
+    try {
+        // Get all posts and JOIN with user data
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+            ],
+        });
 
-    // map through the data, serialize it
+        // map through the data, serialize it
+        const posts = postData.map((post) => post.get({ plain: true }));
 
-    // render appropriate view, sending it the data it needs (which would the the posts)
+        // render appropriate view, sending it the data it needs (which would the the posts)
+        res.render('all-posts', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/post/:id', async (req, res) => {
     // find a Post by Pk
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+          include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+          ],
+        });
 
-    // serialize the data
+        // serialize the data
+        const post = postData.get({ plain: true });
 
     // render appropriate view, sending it the data it needs (which would be the psot)
+        res.render('single-post', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/login', (req, res) => {
     // activity 18, home-routes.js
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login')
 });
 
 router.get('/signup', (req, res) => {
     // activity 18, home-routes.js
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('signup');
 });
 
 module.exports = router;
