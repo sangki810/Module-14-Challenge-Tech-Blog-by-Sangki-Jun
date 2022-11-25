29bd3b945aa219b8ac623388e3a64d74321d49cc
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-11-25 15:00:07 -0500

    updated login.js file

--- public/js/login.js
+++ public/js/login.js
@@ -4,7 +4,7 @@ const loginFormHandler = async (event) => {
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
 
-    if (username && password) {
+    if (email && password) {
         const response = await fetch('/api/user/login', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
